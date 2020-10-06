4f45d7566bc34336d3ef8078dbaa5c3cd51c13d1
Juan Zamorano <jzamoran.ing@gmail.com>
2020-10-05 20:08:17 -0400

    Primera Subida del Proyecto

---
+++ mitest/src/main/java/com/test1/mitest/App.java
@@ -0,0 +1,13 @@
+package com.test1.mitest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Git!" );
+    }
+}
